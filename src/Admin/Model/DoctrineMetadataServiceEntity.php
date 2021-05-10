b8a3e3c4ed37469e4b7a9c5ff52026c7d2c4d234
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:12:00 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Admin/Model/DoctrineMetadataServiceEntity.php
+++ src/Admin/Model/DoctrineMetadataServiceEntity.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas-api-tools/api-tools-doctrine for the canonical source repository
- * @copyright https://github.com/laminas-api-tools/api-tools-doctrine/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas-api-tools/api-tools-doctrine/blob/master/LICENSE.md New BSD License
- */
-
 namespace Laminas\ApiTools\Doctrine\Admin\Model;
 
 use Laminas\Stdlib\ArraySerializableInterface;
